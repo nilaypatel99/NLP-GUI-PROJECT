9d67b8f1ce4537b30696a145c26c19ea9f606981
white.fang <nilp71425@gmail.com>
2023-09-24 12:31:55 +0530

    mainwindowcomplete

---
+++ app.py
@@ -0,0 +1,17 @@
+#Starting app creation using api like SE,NER AND EMOTION DETECTION 
+from tkinter import *
+class NLPApp:
+    def __init__(self):
+        #login ka qui load karna
+        self.root=Tk()   #tk class object
+        self.root.title('sway')
+        self.root.iconbitmap('images/favicon.ico')
+        self.root.geometry('350x600')
+        self.root.configure(bg='gray')
+        self.root.mainloop()
+        
+
+nlp=NLPApp()
+
+
+
