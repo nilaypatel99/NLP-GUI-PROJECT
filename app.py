e29caaee8bb8f5c58b32570b76dd0a90cd201bbf
white.fang <nilp71425@gmail.com>
2023-09-24 14:56:49 +0530

    login_gui finished

--- app.py
+++ app.py
@@ -8,8 +8,42 @@ class NLPApp:
         self.root.iconbitmap('images/favicon.ico')
         self.root.geometry('350x600')
         self.root.configure(bg='gray')
+        self.login_gui()
         self.root.mainloop()
+    
+    def login_gui(self):
+        heading=Label(self.root,text='NLP APP',bg='gray',fg='white')
+        heading.pack(pady=(10,10))
+        heading.configure(font=('verdana',24,'bold'))
         
+        #Email
+        label1=Label(self.root,text='Enter email')    #label for email 
+        label1.pack(pady=(10,10))
+
+        self.email_input=Entry(self.root,width=50) #used entry class to create a box and self cuz we'll use it somewhere
+        self.email_input.pack(pady=(5,10),ipady=4)
+        
+        #Password
+        label2=Label(self.root,text="Enter password")
+        label2.pack(pady=(10,10))
+
+        password=Entry(self.root,width=50)
+        password.pack(pady=(5,10),ipady=4)
+        
+        #Login
+        login=Button(self.root,text='login',width=30,height=2)    #Button class to create login 
+        login.pack(pady=(10,10))
+
+        label3=Label(self.root,text='Not a user ?')
+        label3.pack(pady=(10,10),ipady=4)
+        
+        #Register
+        redirect_btn=Button(self.root,text='Register Now',width=30,height=2,command=self.register_gui)
+        redirect_btn.pack(pady=(10,10))
+        
+        
+        
+
 
 nlp=NLPApp()
 
